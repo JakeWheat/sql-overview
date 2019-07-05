ccdebf2f30dea59021aba8f3c7d1b108b66e1228
Jake Wheat <jakewheatmail@gmail.com>
2019-07-05 19:41:21 +0100

    remove non-ascii chars from the grammar files, tweak website build

--- make_website.sh
+++ make_website.sh
@@ -2,19 +2,16 @@
 
 set -e
 
-cabal sandbox init
-cabal install regex-compat
+cabal v1-sandbox init
+cabal v1-install regex-compat
 
 mkdir -p build
 
 asciidoctor index.asciidoc -o -| runhaskell AddLinks.lhs > build/index.html
 
-gta="runhaskell -package-db=.cabal-sandbox/x86_64-linux-ghc-7.10.2-packages.conf.d/ ./GrammarToAsciidoc.lhs"
+gta="runhaskell -package-db=.cabal-sandbox/x86_64-linux-ghc-8.6.5-packages.conf.d/ ./GrammarToAsciidoc.lhs"
 
-cat sql-92-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-92-grammar.html
-cat sql-1999-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-1999-grammar.html
-cat sql-2003-foundation-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-2003-foundation-grammar.html
-cat sql-2008-foundation-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-2008-foundation-grammar.html
-cat sql-2011-foundation-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-2011-foundation-grammar.html
-cat sql-2011-psm-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-2011-psm-grammar.html
-cat sql-2016-foundation-grammar.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/sql-2016-foundation-grammar.html
+for i in sql-92-grammar sql-1999-grammar sql-2003-foundation-grammar sql-2008-foundation-grammar sql-2011-foundation-grammar sql-2011-psm-grammar sql-2016-foundation-grammar; do
+    echo $i
+    cat $i.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/$i.html
+done
