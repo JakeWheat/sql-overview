ba79ae408b86150272f058729f695586afd0b31c
Jake Wheat <jakewheatmail@gmail.com>
2020-04-19 09:42:33 +0100

    update the build file to use new cabal

--- make_website.sh
+++ make_website.sh
@@ -1,17 +1,20 @@
 #! /usr/bin/env bash
 
-set -e
+# instructions: have a recent version of cabal-install and ghc in your path
+# and perl, asciidoctor
+# run this file and it will generate the html to the build dir
+# tested with cabal-install 3.0.0.0 and ghc 8.10.1
+# should work with earlier versions of both (and later)
+# last ran with asciidoctor 1.5.8
 
-cabal v1-sandbox init
-cabal v1-install regex-compat
+set -e
 
 mkdir -p build
 
-asciidoctor index.asciidoc -o -| runhaskell AddLinks.lhs > build/index.html
-
-gta="runhaskell -package-db=.cabal-sandbox/x86_64-linux-ghc-8.6.5-packages.conf.d/ ./GrammarToAsciidoc.lhs"
+asciidoctor index.asciidoc -o -| cabal run -v0 AddLinks > build/index.html
 
 for i in sql-92-grammar sql-1999-grammar sql-2003-foundation-grammar sql-2008-foundation-grammar sql-2011-foundation-grammar sql-2011-psm-grammar sql-2016-foundation-grammar; do
     echo $i
-    perl fix_lines.pl < $i.txt | $gta | asciidoctor - | runhaskell AddLinks.lhs > build/$i.html
+    perl fix_lines.pl < $i.txt | cabal run -v0 GrammarToAsciidoc | asciidoctor - | cabal run -v0 AddLinks > build/$i.html
 done
+
